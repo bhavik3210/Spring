95b814464b76ca1c723abea844590c4a93b2802c
Bhavik <bhavik@mail.com>
2022-07-15 18:29:42 -0500

    added beans

--- spring-5-fundamentals/conference-java/src/main/java/com/pluralsight/Application.java
+++ spring-5-fundamentals/conference-java/src/main/java/com/pluralsight/Application.java
@@ -1,12 +1,16 @@
 package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String args[]) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        SpeakerService service = new SpeakerServiceImpl(); // we don't need this anymore because using bean factory to create instances in AppConfig file
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- spring-5-fundamentals/conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ spring-5-fundamentals/conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,17 +1,20 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
