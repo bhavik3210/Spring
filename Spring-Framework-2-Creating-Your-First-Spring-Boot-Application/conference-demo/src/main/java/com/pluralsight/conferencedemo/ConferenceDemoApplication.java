fcc022bdde92457f4c59b597daad816048af4a10
Bhavik <bhavik@mail.com>
2022-07-18 22:00:07 -0500

    updated package name

--- Spring-Framework-2-Creating-Your-First-Spring-Boot-Application/conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ Spring-Framework-2-Creating-Your-First-Spring-Boot-Application/conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Spring-Framework-2-Creating-Your-First-Spring-Boot-Application/conference-demo/src/test/java/com/pluralsight/conferencedemo/ConferenceDemoApplicationTests.java
+++ Spring-Framework-2-Creating-Your-First-Spring-Boot-Application/conference-demo/src/test/java/com/pluralsight/conferencedemo/ConferenceDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.conferencedemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
